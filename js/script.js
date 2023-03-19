d338002240c373dd81b5d15f954c83078946caea
Scott Atkinson <scott.atkin79@gmail.com>
2023-03-19 20:30:53 +0000

    create multiple enemies and have them follow a way point

--- js/script.js
+++ js/script.js
@@ -24,6 +24,11 @@ class Enemy {
         this.position = position
         this.width = 100
         this.height = 100
+        this.waypointIndex = 0
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2
+        }
     }
 
     draw() {
@@ -33,21 +38,47 @@ class Enemy {
 
     update() {
         this.draw()
-        this.position.x += 1
+
+        const waypoint = waypoints[this.waypointIndex]
+        const yDistance = waypoint.y - this.center.y
+        const xDistance = waypoint.x - this.center.x
+        const angle = Math.atan2(yDistance, xDistance)
+        this.position.x += Math.cos(angle)
+        this.position.y += Math.sin(angle)
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2
+        }
+
+        if (
+            Math.round(this.center.x) === Math.round(waypoint.x) &&
+            Math.round(this.center.y) === Math.round(waypoint.y) &&
+            this.waypointIndex < waypoints.length - 1
+        ) {
+            this.waypointIndex++
+        }
     }
 }
 
-const enemy = new Enemy({
-    position: {
-        x: 200,
-        y: 400
-    }
-})
+const enemies = []
+for (let i = 1; i < 10; i++) {
+    const xOffset = i * 150
+    enemies.push(
+        new Enemy({
+            position: {
+                x: waypoints[0].x - xOffset,
+                y: waypoints[0].y
+            }
+        })
+    )
+    console.log(waypoints[0].x - xOffset)
+}
 
 function animate() {
     requestAnimationFrame(animate)
-    console.log('GO!!')
 
     c.drawImage(image, 0, 0)
-    enemy.update()
+    enemies.forEach((enemy) => {
+        enemy.update()
+    })
 }
