c747799027d6000a0d1124b59005aa4d6dce7e9b
Scott Atkinson <scott.atkin79@gmail.com>
2023-03-20 21:02:36 +0000

    added touch listener

--- js/script.js
+++ js/script.js
@@ -207,3 +207,22 @@ window.addEventListener('mousemove', (event) => {
         }
     }
 })
+
+window.addEventListener('touchmove', (event) => {
+    mouse.x = event.clientX
+    mouse.y = event.clientY
+
+    activeTile = null
+    for (let i = 0; i < placementTiles.length; i++) {
+        const tile = placementTiles[i]
+        if (
+            mouse.x > tile.position.x &&
+            mouse.x < tile.position.x + tile.size &&
+            mouse.y > tile.position.y &&
+            mouse.y < tile.position.y + tile.size
+        ) {
+            activeTile = tile
+            break
+        }
+    }
+})
